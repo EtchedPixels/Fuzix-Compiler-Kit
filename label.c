7a78e1297e59e89f8d221e100d8129ca9d20c935
Alan Cox <etchedpixels@gmail.com>
2022-05-06 11:35:23 +0100

    label: support files for goto/label

---
+++ label.c
@@ -0,0 +1,60 @@
+/*
+ *	Labels look like symbols except that they don't really behave
+ *	like them in terms of scope. Handle them on their own
+ */
+
+#include <stdint.h>
+#include "compiler.h"
+
+#define MAXLABEL	16
+
+#define L_DECLARED	0x8000
+
+struct label {
+    uint16_t name;
+    uint16_t line;
+};
+
+struct label labels[MAXLABEL];
+struct label *labelp;
+
+void init_labels(void)
+{
+    labelp = labels;
+}
+
+static void new_label(unsigned n)
+{
+    if (labelp == &labels[MAXLABEL])
+        error("too many goto labels");
+    labelp->name = n;
+    labelp->line = line_num;
+    labelp++;
+}
+
+void use_label(unsigned n)
+{
+    struct label *p = labels;
+    n &= 0x7FFF;
+    while(p < labelp) {
+        if (n == (p->name & 0x7FFF))
+            return;
+        p++;
+    }
+    new_label(n);
+}
+
+void add_label(unsigned n)
+{
+    new_label(n | L_DECLARED);
+}
+
+void check_labels(void)
+{
+    struct label *p = labels;
+    while(p < labelp) {
+        if (!(p->name & L_DECLARED))
+            errorline(p->line, "unknown label");
+        p++;
+    }
+}
