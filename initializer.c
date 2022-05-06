920498566c436924cf48d59457482b7964f616de
Alan Cox <etchedpixels@gmail.com>
2022-05-06 10:51:19 +0100

    initializer: put most of the remaining pieces into place
    The stuff we pass between stages for this is a bit bulky but does work. Needs
    refining once it all works.

--- initializer.c
+++ initializer.c
@@ -1,34 +1,28 @@
-/*
- *	Initializers .. really needs the tree consting stuff doing to work
- *	properly.
- */
-
 #include "compiler.h"
 
-/* This is a bit trickier but we need our tree constifier to do it right */
-
-/* A single initialization value */
+/*
+ *	Write a single initialization element to the stream. For auto variables
+ *	we generate the assignment tree, for static or globals we generate a
+ *	stream of data with types for the backend.
+ */
 static void initializer_single(struct symbol *sym, unsigned type, unsigned storage)
 {
-    int v;
-    /* Will be a coerce(constify(expression)()) - need to handle pointers
-       and "string" also handle names being counted as constant in this
-       situation */
-    struct node *n = constant_node();
+    struct node *n = expression_tree(0);
     /* Need to make constant_node do its own casting TODO */
     if (storage == AUTO) {
         write_tree(tree(T_EQ, make_symbol(sym), n));
     } else {
-        /* TODO */
-//    put_typed_data(type, v, storage);
-       ;
+        put_typed_data(n, storage);
     }
+    free_tree(n);
 }
 
 /* C99 permits trailing comma and ellipsis */
 /* Strictly {} is not permitted - there must be at least one value */
 
-/* Array initializer. Repeated runs of the same type */
+/*
+ *	Array bottom level initializer: repeated runs of the same type
+ */
 static void initializer_group(struct symbol *sym, unsigned type, unsigned n, unsigned storage)
 {
     /* Up to n comma delimited initializers of a type */
@@ -45,12 +39,24 @@ static void initializer_group(struct symbol *sym, unsigned type, unsigned n, uns
     }
     if (n) {
         unsigned s = type_sizeof(type) * n;
-//        put_padding(s, storage);
+        put_padding_data(s, storage);
     }
+    /* Catches any excess elements */
     require(T_RCURLY);
 }
 
-/* Struct and union initializer */
+/*
+ *	Struct and union initializer
+ *
+ *	This is similar to an array but each element has its own expected
+ *	type, and some elements may themselves be structures or arrays. It's
+ *	mostly recursion.
+ *
+ *	Remaining space in the object is padded.
+ *
+ *	We don't deal with auto here as with arrays because we don't support
+ *	the C extensions of auto array and struct with initializers.
+ */
 static void initializer_struct(struct symbol *psym, unsigned type, unsigned storage)
 {
     struct symbol *sym = symbol_ref(type);
@@ -66,25 +72,34 @@ static void initializer_struct(struct symbol *psym, unsigned type, unsigned stor
     while(n-- && token != T_RCURLY) {
         type = p[1];
         p += 3;
+        if (sym->storage == S_STRUCT)
+            s -= type_sizeof(type);
         initializers(psym, type, storage);
         if (!match(T_COMMA))
             break;
     }
+    if (n == -1 && token != T_RCURLY)
+        error("too many initializers");
     require(T_RCURLY);
     /* For a union zerofill the slack if other elements are bigger */
     /* For a struct fill from the offset of the next field to the size of
        the base object */
-#if 0
     if (sym->storage == S_UNION)
-        put_padding(s - type_sizeof(type), storage);
+        put_padding_data(s - type_sizeof(type), storage);
     else if (n)
-        put_padding(s - p[2], storage);	/* From offset of field to end */
-#endif
+        put_padding_data(s - p[2], storage);	/* From offset of field to end */
 }
 
+/*
+ *	Array initializer.
+ *
+ *	We recursively call down through the layers until we hit the bottom
+ *	layer of the array which should be a series of values in the type
+ *	of the array. The base value may be a structure.
+ */
 static void initializer_array(struct symbol *sym, unsigned type, unsigned depth, unsigned storage)
 {
-    unsigned n = array_dimension(n, depth);
+    unsigned n = array_dimension(type, depth);
     if (depth < array_num_dimensions(type)) {
         require(T_LCURLY);
         while(n--)
@@ -94,10 +109,13 @@ static void initializer_array(struct symbol *sym, unsigned type, unsigned depth,
         if (IS_STRUCT(type) && !PTR(type))
             initializer_struct(sym, type, storage);
         else
-            initializer_group(sym, type, array_dimension(type, depth), storage);
+            initializer_group(sym, type, n, storage);
     }
 }
 
+/*
+ *	Initialize an object.
+ */
 void initializers(struct symbol *sym, unsigned type, unsigned storage)
 {
     /* FIXME: review pointer rule */
