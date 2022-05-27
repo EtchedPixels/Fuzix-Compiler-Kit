b05073bcf7b55036acc114db459a1d9ad4480921
Alan Cox <etchedpixels@gmail.com>
2022-05-27 02:41:23 +0100

    initializer: support variable length array initialization
    Also allow for the dangling comma as the standard permits
    Allow string initializers to be bracketed or not (nothing like consistency and logic is there)

--- initializer.c
+++ initializer.c
@@ -21,26 +21,50 @@ static void initializer_single(struct symbol *sym, unsigned type, unsigned stora
 /* C99 permits trailing comma and ellipsis */
 /* Strictly {} is not permitted - there must be at least one value */
 
+static unsigned initializer_string(unsigned n)
+{
+        /* This one is weird because the string is not literal */
+        if (n)
+            n = copy_string(0, n, 1, 0);
+        else
+            n = copy_string(0, TARGET_MAX_PTR, 0, 0);
+        return n;
+}
+
 /*
  *	Array bottom level initializer: repeated runs of the same type
  *
  *	TODO: In theory we could have a platform that needs padding
  *	and we don't deal with that aspect of alignment yet
  */
-static void initializer_group(struct symbol *sym, unsigned type, unsigned n, unsigned storage)
+static unsigned initializer_group(struct symbol *sym, unsigned type, unsigned n, unsigned storage)
 {
+    unsigned sized = n;
+    unsigned string = 0;
     /* C has a funky special case rule that you can write
        char x[16] = "foo"; which creates a copy of the string in that
-       array not a literal reference */
+       array not a literal reference. It's also got a second funky special case
+       rule that you can write { "string" }. */
+
+    if ((type_canonical(type) & ~UNSIGNED) == CCHAR)
+        string = 1;
+
     if (token == T_STRING) {
-        if ((type_canonical(type) & ~UNSIGNED) != CCHAR)
+        if (!string)
             typemismatch();
-        /* This one is weird because the string is not literal */
-        copy_string(0, n, 1, 0);
-        return;
+        return initializer_string(n);
     }
     require(T_LCURLY);
+    if (!sized)
+        n = TARGET_MAX_PTR;
     while(n && token != T_RCURLY) {
+        /* Deal with the second string special case, gotta love C some days */
+        if (token == T_STRING && string) {
+            n = initializer_string(sized);
+            require(T_RCURLY);
+            return n;
+        }
+        string = 0;	/* Only valid first */
         if (token == T_ELLIPSIS)
             break;
         n--;
@@ -48,12 +72,13 @@ static void initializer_group(struct symbol *sym, unsigned type, unsigned n, uns
         if (!match(T_COMMA))
             break;
     }
-    if (n) {
+    if (n && sized) {
         unsigned s = type_sizeof(type) * n;
         put_padding_data(s);
     }
     /* Catches any excess elements */
     require(T_RCURLY);
+    return n;
 }
 
 /*
@@ -124,19 +149,25 @@ static void initializer_array(struct symbol *sym, unsigned type, unsigned depth,
     if (depth < array_num_dimensions(type)) {
         type = type_deref(type);
         require(T_LCURLY);
+        if (n == 0)
+            n = TARGET_MAX_PTR;
         while(n--) {
             initializer_array(sym, type, depth + 1, storage);
-            if (n && match(T_COMMA))
+            /* Trailing comma is allowed so eat it before checking n */
+            if (match(T_COMMA) && n)
                 continue;
             break;
         }
+        if (array_dimension(type, depth) == 0)
+            sym->type = array_with_size(type, n);
         /* Pad the remaining pieces */
         while(n--)
             put_padding_data(type_sizeof(type));
         require(T_RCURLY);
     } else {
-        type = type_deref(type);
-        initializer_group(sym, type, n, storage);
+        n = initializer_group(sym, type_deref(type), n, storage);
+        if (array_dimension(type, depth) == 0)
+            sym->type = array_with_size(type, n);
     }
 }
 
--- target.h
+++ target.h
@@ -1,6 +1,7 @@
 #define TARGET_MAX_INT		32767L
 #define TARGET_MAX_LONG		2147483647UL	/* and a double persenne prime too */
 #define TARGET_MAX_UINT		65535UL
+#define TARGET_MAX_PTR		TARGET_MAX_UINT
 
 #define TARGET_CHAR_MASK	0x00FFU
 #define TARGET_SHORT_MASK	0xFFFFU
