c7f1dfbfa1fb0b392bcac2d9c121ad88c0e39ef9
Alan Cox <etchedpixels@gmail.com>
2022-06-16 11:26:22 +0100

    6803: create a blank slate 6803 target

---
+++ backend-6803.c
@@ -0,0 +1,313 @@
+/*
+ *	The 680x/630x series processors are closely related except for the
+ *	6809/6309 which is totally different and to us unrelated. All of them
+ *	have a very small number of registers and an instruction set that is
+ *	built around register/memory operations.
+ *
+ *	The original 6800 cannot push/pop the index register and has no 16bit
+ *	maths operations. It is not considerded in this target at this point
+ *
+ *	The 6803 has a single index register and index relative addressing.
+ *	There is almost no maths on the index register except an 8bit add.
+ *	The processor also lacks the ability to do use the stack as an index
+ *	but can copy the stack pointer to x in one instruction. Thus we have
+ *	to play tracking games on the contents of X.
+ *
+ *	The 6303 is slightly pipelined and adds some bit operations that are
+ *	mostly not relative to the compiler as well as a the ability to exchange
+ *	X and D, which makes maths on the index much easier.
+ *
+ *	The 68HC11 adds an additional Y index register, which has a small
+ *	performance penalty. For now we ignore Y and treat it as a 6803.
+ *
+ *	All conditional branches are short range, the assemnbler supports
+ *	jxx which turns into the correct instruction combination for a longer
+ *	conditional branch as needed.
+ *
+ *	Almsot every instruction changes the flags. This helps us hugely in
+ *	most areas and is a total nightmare in a couple.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "compiler.h"
+#include "backend.h"
+
+#define BYTE(x)		(((unsigned)(x)) & 0xFF)
+#define WORD(x)		(((unsigned)(x)) & 0xFFFF)
+
+/*
+ *	State for the current function
+ */
+static unsigned frame_len;	/* Number of bytes of stack frame */
+static unsigned sp;		/* Stack pointer offset tracking */
+
+/*
+ *	Our chance to do tree rewriting. We don't do much for the 8080
+ *	at this point, but we do rewrite name references and function calls
+ *	to make them easier to process.
+ */
+struct node *gen_rewrite_node(struct node *n)
+{
+	return n;
+}
+
+/* Export the C symbol */
+void gen_export(const char *name)
+{
+	printf("	.export _%s\n", name);
+}
+
+void gen_segment(unsigned s)
+{
+	switch(s) {
+	case A_CODE:
+		printf("\t.text\n");
+		break;
+	case A_DATA:
+	case A_LITERAL:
+		printf("\t.data\n");
+		break;
+	case A_BSS:
+		printf("\t.bss\n");
+		break;
+	default:
+		error("gseg");
+	}
+}
+
+void gen_prologue(const char *name)
+{
+	printf("_%s:\n", name);
+}
+
+/* Generate the stack frame */
+void gen_frame(unsigned size)
+{
+	frame_len = size;
+	sp += size;
+	gen_helpcall(NULL);
+	printf("enter\n");
+	gen_value(UINT, size);
+}
+
+void gen_epilogue(unsigned size)
+{
+	if (sp != size) {
+		error("sp");
+	}
+	sp -= size;
+	gen_helpcall(NULL);
+	printf("exit\n");
+	gen_value(UINT, size);
+	printf("\tret\n");
+}
+
+void gen_label(const char *tail, unsigned n)
+{
+	printf("L%d%s:\n", n, tail);
+}
+
+void gen_jump(const char *tail, unsigned n)
+{
+	printf("\tjmp L%d%s\n", n, tail);
+}
+
+void gen_jfalse(const char *tail, unsigned n)
+{
+	printf("\tjz L%d%s\n", n, tail);
+}
+
+void gen_jtrue(const char *tail, unsigned n)
+{
+	printf("\tjnz L%d%s\n", n, tail);
+}
+
+void gen_switch(unsigned n, unsigned type)
+{
+	gen_helpcall(NULL);
+	printf("switch");
+	helper_type(type, 0);
+	printf("\n\t.word Sw%d\n", n);
+}
+
+void gen_switchdata(unsigned n, unsigned size)
+{
+	printf("Sw%d:\n", n);
+	printf("\t.word %d\n", size);
+}
+
+void gen_case(unsigned tag, unsigned entry)
+{
+	printf("Sw%d_%d:\n", tag, entry);
+}
+
+void gen_case_label(unsigned tag, unsigned entry)
+{
+	printf("Sw%d_%d:\n", tag, entry);
+}
+
+void gen_case_data(unsigned tag, unsigned entry)
+{
+	printf("\t.word Sw%d_%d\n", tag, entry);
+}
+
+void gen_helpcall(struct node *n)
+{
+	printf("\thjsr __");
+}
+
+void gen_helpclean(struct node *n)
+{
+}
+
+void gen_data_label(const char *name, unsigned align)
+{
+	printf("_%s:\n", name);
+}
+
+void gen_space(unsigned value)
+{
+	printf("\t.ds %d\n", value);
+}
+
+void gen_text_data(unsigned n)
+{
+	printf("\t.word T%d\n", n);
+}
+
+void gen_literal(unsigned n)
+{
+	if (n)
+		printf("T%d:\n", n);
+}
+
+void gen_name(struct node *n)
+{
+	printf("\t.word _%s+%d\n", namestr(n->snum), WORD(n->value));
+}
+
+void gen_value(unsigned type, unsigned long value)
+{
+	if (PTR(type)) {
+		printf("\t.word %u\n", (unsigned) value);
+		return;
+	}
+	switch (type) {
+	case CCHAR:
+	case UCHAR:
+		printf("\t.byte %u\n", (unsigned) value & 0xFF);
+		break;
+	case CSHORT:
+	case USHORT:
+		printf("\t.word %d\n", (unsigned) value & 0xFFFF);
+		break;
+	case CLONG:
+	case ULONG:
+	case FLOAT:
+		/* We are big endian */
+		printf("\t.word %d\n", (unsigned) ((value >> 16) & 0xFFFF));
+		printf("\t.word %d\n", (unsigned) (value & 0xFFFF));
+		break;
+	default:
+		error("unsuported type");
+	}
+}
+
+void gen_start(void)
+{
+	printf("\t.code\n");
+}
+
+void gen_end(void)
+{
+}
+
+void gen_tree(struct node *n)
+{
+	codegen_lr(n);
+	printf(";\n");
+}
+
+/*
+ *	All our sizes are fairly predictable. Our stack is byte oriented
+ *	so we push bytes as bytes.
+ */
+static unsigned get_size(unsigned t)
+{
+	if (PTR(t))
+		return 2;
+	if (t == CSHORT || t == USHORT)
+		return 2;
+	if (t == CCHAR || t == UCHAR)
+		return 1;
+	if (t == CLONG || t == ULONG || t == FLOAT)
+		return 4;
+	if (t == CLONGLONG || t == ULONGLONG || t == DOUBLE)
+		return 8;
+	if (t == VOID)
+		return 0;
+	error("gs");
+	return 0;
+}
+
+static unsigned get_stack_size(unsigned t)
+{
+	return get_size(t);
+}
+
+unsigned gen_push(struct node *n)
+{
+	/* Our push will put the object on the stack, so account for it */
+	sp += get_stack_size(n->type);
+	return 0;
+}
+
+/*
+ *	If possible turn this node into a direct access. We've already checked
+ *	that the right hand side is suitable. If this returns 0 it will instead
+ *	fall back to doing it stack based.
+ */
+unsigned gen_direct(struct node *n)
+{
+	switch(n->op) {
+	/* Clean up is special and must be handled directly. It also has the
+	   type of the function return so don't use that for the cleanup value
+	   in n->right */
+	case T_CLEANUP:
+		gen_helpcall(NULL);
+		printf("cleanup\n");
+		gen_value(UINT, n->right->value);
+		sp -= n->right->value;
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ *	Allow the code generator to shortcut the generation of the argument
+ *	of a single argument operator (for example to shortcut constant cases
+ *	or simple name loads that can be done better directly)
+ */
+unsigned gen_uni_direct(struct node *n)
+{
+	return 0;
+}
+
+/*
+ *	Allow the code generator to shortcut trees it knows
+ */
+unsigned gen_shortcut(struct node *n)
+{
+	return 0;
+}
+
+unsigned gen_node(struct node *n)
+{
+	/* Function call arguments are special - they are removed by the
+	   act of call/return and reported via T_CLEANUP */
+	if (n->left && n->op != T_ARGCOMMA && n->op != T_FUNCCALL)
+		sp -= get_stack_size(n->left->type);
+	return 0;
+}
