9c903970b32c35cd8d27ac3fffb9c9042191df39
Alan Cox <etchedpixels@gmail.com>
2022-06-19 15:03:27 +0100

    body: add flag info for backend to function body info

--- body.h
+++ body.h
@@ -1,3 +1,6 @@
 extern void statement_block(unsigned need_brack);
 extern void function_body(unsigned st, unsigned name, unsigned type);
 
+#define F_VOIDRET		1
+#define F_VOID			2
+#define F_VARARG		4
