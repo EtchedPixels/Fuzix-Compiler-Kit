014dcbbff23eb5ec68739f699324e652fd28c076
Alan Cox <etchedpixels@gmail.com>
2023-02-21 21:34:45 +0000

    bitops: test more cases

--- test/tests/0006-bitops.c
+++ test/tests/0006-bitops.c
@@ -19,6 +19,21 @@ unsigned bitflip(unsigned a)
     return ~a;
 }
 
+unsigned andeq(unsigned a, unsigned b)
+{
+    return a &= b;
+}
+
+unsigned oreq(unsigned a, unsigned b)
+{
+    return a |= b;
+}
+
+unsigned xoreq(unsigned a, unsigned b)
+{
+    return a ^= b;
+}
+
 int main(int argc, char *argv[])
 {
     if (and(4, 5) != 4)
@@ -29,5 +44,11 @@ int main(int argc, char *argv[])
         return 3;
     if (bitflip(0xAA) != 0xFF55)
         return 4;
+    if (andeq(4, 5) != 4)
+        return 5;
+    if (xoreq(128, 192) != 64)
+        return 6;
+    if (oreq(0x55, 0xAA) != 0xFF)
+        return 7;
     return 0;
 }
