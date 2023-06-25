8240222131b1635e1d8ea393bad4ecdaf9dd8560
Alan Cox <etchedpixels@gmail.com>
2023-06-25 20:29:50 +0100

    tests: correct register test

--- test/tests/0601-register.c
+++ test/tests/0601-register.c
@@ -1,10 +1,23 @@
+static unsigned char byteop(void)
+{
+    register unsigned char i = 16;
+
+    while (i--);
+    return i;
+    /* buf[i] = rj_sbox(buf[i]); */
+} /* aes_subBytes */
+
+
+
 int main(int argc, char *argv[])
 {
     register int x = 0;
     if (x != 0)
         return 1;
     while(x++ < 30);
-    if (x != 30)
-        return 0;
+    if (x != 31)
+        return 2;
+    if (byteop() != 0xFF)
+        return 3;
     return 0;
 }
