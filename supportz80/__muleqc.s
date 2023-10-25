24ff277fb0c3e0f38948affa344964497a37fef4
Alan Cox <etchedpixels@gmail.com>
2023-10-25 14:52:28 +0100

    z80: fix missing __muleqc helper

---
+++ supportz80/__muleqc.s
@@ -0,0 +1,23 @@
+;
+;		(TOS) *= HL
+;
+		.export __muleqc
+		.code
+
+__muleqc:
+		ex	de,hl
+		pop	hl
+		ex	(sp),hl
+		; Now we are doing (HL) * DE
+		push	de
+		ld	e,(hl)
+		ld	d,0
+		ex	(sp),hl
+		; We are now doing HL * DE and the address we want is TOS
+		call	__mulde
+		; Return is in HL
+		ex	de,hl
+		pop	hl
+		ld	(hl),e
+		ex	de,hl
+		ret
