b7d9b918a061bfc739eedabb89be1beabf162675
Alan Cox <etchedpixels@gmail.com>
2023-10-30 22:08:49 +0000

    tree: add a flag to indicate cases where we can't play with CC flag tricks
    Because of the way && and || logic heirarchies are generated we can't muck
    with the ccflags in those cases. Fixing that requires some changes to how
    the whole thing is structured so we don't chain conditional branches. That
    is decidedly non trivial so can wait

--- tree.h
+++ tree.h
@@ -12,6 +12,7 @@ struct node
 #define IMPURE			16	/* Something in the subtree has a side effect */
 #define CCONLY			32	/* Only need condition side effects if platform has cc based branching */
 #define NEEDCC			64	/* Node needs the cc setting behaviour */
+#define CCFIXED			128	/* CC flags must match the expected default */
     unsigned long value;	/* Offset for a NAME fp offset for a LOCAL */
     unsigned snum;		/* Name of symbol (for code generator) */
     unsigned val2;		/* Label for name, (also used for code gen) */
