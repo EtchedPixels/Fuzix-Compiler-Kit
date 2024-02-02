6ae45206db3fae955a0869cc39adc92bc196ce91
Alan Cox <etchedpixels@gmail.com>
2024-02-02 19:28:47 +0000

    6502: fix test run

--- test/run-test6502.sh
+++ test/run-test6502.sh
@@ -4,6 +4,6 @@ do
 	b=$(basename $i .c)
 	echo  $b":"
 	fcc -m6502 -c tests/$b.c
-	ld6502 -b -C512 testcrt0.o tests/$b.o -o tests/$b /opt/fcc/lib/6502/lib6502.a -m tests/$b.map
+	ld6502 -b -C512 testcrt0_6502.o tests/$b.o -o tests/$b /opt/fcc/lib/6502/lib6502.a -m tests/$b.map
 	./emu6502 tests/$b tests/$b.map
 done
