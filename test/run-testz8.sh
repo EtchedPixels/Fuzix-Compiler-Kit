1e13cad18c25d20a620e23adf636a5b49dcbb43a
Alan Cox <etchedpixels@gmail.com>
2024-02-09 22:04:55 +0000

    test: z8 scripts

---
+++ test/run-testz8.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+for i in tests/*.c
+do
+	b=$(basename $i .c)
+	echo  $b":"
+	fcc -mz8 -c tests/$b.c
+	ldz8 -b -C0 testcrt0_z8.o tests/$b.o -o tests/$b /opt/fcc/lib/z8/libz8.a -m tests/$b.map
+	./emuz8 tests/$b tests/$b.map
+done
