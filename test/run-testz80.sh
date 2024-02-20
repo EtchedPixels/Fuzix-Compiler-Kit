271bcf5f893694acca24ed45509888d8a8037381
Alan Cox <etchedpixels@gmail.com>
2024-02-20 19:25:43 +0000

    test: switch to new style naming and layout

--- test/run-testz80.sh
+++ test/run-testz80.sh
@@ -3,7 +3,7 @@ for i in tests/*.c
 do
 	b=$(basename $i .c)
 	echo  $b":"
-	ccz80 -O -mz80 -c tests/$b.c
+	fcc -O -mz80 -c tests/$b.c
 	ldz80 -b -C0 testcrtz80.o tests/$b.o -o tests/$b /opt/ccz80/lib/libz80.a -m tests/$b.map
 	./emuz80 tests/$b tests/$b.map
 done
