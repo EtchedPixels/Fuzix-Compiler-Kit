94295bd08494ab87593fdf5eac37e604769c7bce
Alan Cox <etchedpixels@gmail.com>
2024-04-08 13:40:05 +0100

    6809: update testcrt0 to match support

--- test/testcrt0_6809.s
+++ test/testcrt0_6809.s
@@ -1,14 +1,27 @@
 	.dp
 
 	.export hireg
-	.export tmp
 	.export zero
 	.export	one
 
+	.export hireg
+	.export zero
+	.export one
+
 hireg:	.word	0
+zero:	.byte	0	; overlaps 1
+one:	.word	0
+
+	.export tmp
+	.export tmp2
+	.export tmp3
+	.export tmp4
+
 tmp:	.word	0
-zero:	.word	0
-one:	.word	1
+tmp2:	.word	0
+tmp3:	.word	0
+tmp4:	.word	0
+
 
 	.code ; (at 0x0100)
 
