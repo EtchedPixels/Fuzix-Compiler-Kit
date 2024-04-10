d0c380ed71524eddc217719925c3a4d17378c512
Alan Cox <etchedpixels@gmail.com>
2024-04-10 09:26:32 +0100

    6809: fix __xmuleq[u]c

--- support6809/__xmuleqc.s
+++ support6809/__xmuleqc.s
@@ -1,14 +1,18 @@
 ;
 ;	,X *= D
 ;
-	.export __xmuleq
+	.export __xmulequc
+	.export __xmuleqc
 
-__xmuleq:
+__xmuleqc:
+__xmulequc:
+	;	Only the low bytes of the input matter
 	pshs x		; Save the address
-	ldx ,x		; Get the ,X value
+	lda ,x		; Get the ,X value
+	pshs a,cc	; Stack it and a spare random byte
 	pshs x		; onto the stack
 	jsr __mul	; Do the multiply, D gets result
 	puls x		; Remove arg from stack
 	puls x		; Get the address
-	std ,x		; Save result
+	stb ,x		; Save result
 	rts
