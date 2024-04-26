ab1eecbf8d8e1cb160485651bdd1ff93b0e78df9
Alan Cox <etchedpixels@gmail.com>
2024-04-26 10:51:30 +0100

    z8: link with the registers used for fast calling clear of the workspace

--- test/run-testz8.sh
+++ test/run-testz8.sh
@@ -4,6 +4,6 @@ do
 	b=$(basename $i .c)
 	echo  $b":"
 	fcc -mz8 -c tests/$b.c
-	ldz8 -b -C0 testcrt0_z8.o tests/$b.o -o tests/$b /opt/fcc/lib/z8/libz8.a -m tests/$b.map
+	ldz8 -b -C0 -Z48 testcrt0_z8.o tests/$b.o -o tests/$b /opt/fcc/lib/z8/libz8.a -m tests/$b.map
 	./emuz8 tests/$b tests/$b.map
 done
