d5c464b27576f3c4fd6b804f7f3ec9f98f8107ca
Alan Cox <etchedpixels@gmail.com>
2024-04-26 21:30:35 +0100

    run-test: still referenced old path

--- test/run-test.sh
+++ test/run-test.sh
@@ -3,7 +3,7 @@ for i in tests/*.c
 do
 	b=$(basename $i .c)
 	echo  $b":"
-	fcc -m8085 -c tests/$b.c
-	ld8080 -b -C0 testcrt0.o tests/$b.o -o tests/$b /opt/fcc/lib/8080/lib8085.a -m tests/$b.map
+	fcc  -Os -m8085 -c tests/$b.c
+	ld8080 -b -C0 testcrt0.o tests/$b.o -o tests/$b /opt/fcc/lib/8085/lib8085.a -m tests/$b.map
 	./emu85 tests/$b tests/$b.map
 done
