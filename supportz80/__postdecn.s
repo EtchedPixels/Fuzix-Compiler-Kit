ee48cdab3aae459a2b1f058de462a1b33060f3a3
Alan Cox <etchedpixels@gmail.com>
2024-05-10 11:51:59 +0100

    z80: remove duplicate implementations of postdec1/2

--- supportz80/__postdecn.s
+++ supportz80/__postdecn.s
@@ -1,12 +1,9 @@
-		.export __postdec1
-		.export __postdec2
 		.export __postdec1d
 		.export __postdec2d
 		.code
 
 __postdec1d:
 		ex	de,hl
-__postdec1:
 		ld	e,(hl)
 		inc	hl
 		ld	d,(hl)
@@ -20,7 +17,6 @@ __postdec1:
 
 __postdec2d:
 		ex	de,hl
-__postdec2:
 		ld	e,(hl)
 		inc	hl
 		ld	d,(hl)
