ae5cc4d7fbd72f96cfa0c9c90263c5d06d10d879
Alan Cox <etchedpixels@gmail.com>
2024-05-11 17:43:22 +0100

    nova: add shift operators

---
+++ supportnova3/__shift.s
@@ -0,0 +1,78 @@
+	.export f__shl
+	.export f__shr
+	.export f__shru
+	.export f__shll
+	.export f__shrl
+	.export f__shrul
+
+f__shl:
+	neg	1,0		; no dec but we can negate and move in one
+	popa	1		; so who cares 8)
+shllp:
+	movzl	1,1
+	inc	0,0,szr
+	jmp	shllp,1
+done:	sta	3,__tmp,0
+	popa	0		; clean stack
+	jmp	@__tmp,0
+
+f__shru:
+	neg	1,0
+	popa	1
+shrulp:
+	movzr	1,1
+	inc	0,0,szr
+	jmp	shrulp,1
+	jmp	done,1
+
+f__shr:
+	neg	1,0
+	popa	1
+	; SHR is more interesting as we need to handle sign extension
+	movl#	1,1,snr	; get top bit into carry
+	jmp	shrulp,1	; starts with a zero bit so use shru
+shrlp:
+	movor	1,1	; shift right and set high bit
+	inc	0,0,szr
+	jmp	shrlp,1
+	jmp	done,1
+
+;	Same idea but 32bit wide
+
+f__shll:
+	neg	1,0
+	popa	2		; high
+via_shll:
+	popa	1		; low
+llp:
+	movzl	1,1		; shift left low fill 0
+	movl	2,2		; and upper half
+	inc	0,0,szr
+	jmp	llp,1
+	sta	2,__hireg,0
+	jmp	done,1
+
+f__shrul:
+	neg	1,0
+	popa	2
+	popa	1
+rlp:
+	movzr	2,2
+	movr	1,1
+	inc	0,0,szr
+	jmp	rlp,1
+	sta	2,__hireg,0
+	jmp	done,1
+	
+f__shrl:
+	neg	1,0
+	popa	2
+	movl#	2,2,snr
+	jmp	via_shll,1
+r1lp:
+	movor	2,2
+	movr	1,1
+	inc	0,0,szr
+	jmp	r1lp,1
+	sta	2,__hireg,0
+	jmp	done,1
