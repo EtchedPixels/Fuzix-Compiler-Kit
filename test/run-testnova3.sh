384dbc32068e5fa10604748399b926baeb0e73e4
Alan Cox <etchedpixels@gmail.com>
2024-06-04 12:21:18 +0100

    nova: start testing optimization peepholes too

--- test/run-testnova3.sh
+++ test/run-testnova3.sh
@@ -3,7 +3,7 @@ for i in tests/*.c
 do
 	b=$(basename $i .c)
 	echo  $b":"
-	fcc  -mnova3 -c tests/$b.c
+	fcc -O2 -mnova3 -c tests/$b.c
 	ldnova -b -C512 testcrt0_nova3.o tests/$b.o -o tests/$b /opt/fcc/lib/nova/libnova.a -m tests/$b.map
 	./nova tests/$b tests/$b.map
 done
