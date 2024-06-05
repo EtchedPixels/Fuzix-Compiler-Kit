7b2e3d49a78da45deb674ff187714031ae64d51a
Alan Cox <etchedpixels@gmail.com>
2024-06-05 15:18:11 +0100

    nova3: update paths for nova3

--- test/run-testnova3.sh
+++ test/run-testnova3.sh
@@ -4,6 +4,6 @@ do
 	b=$(basename $i .c)
 	echo  $b":"
 	fcc -O2 -mnova3 -c tests/$b.c
-	ldnova -b -C512 testcrt0_nova3.o tests/$b.o -o tests/$b /opt/fcc/lib/nova/libnova.a -m tests/$b.map
+	ldnova -b -C512 -Z80 testcrt0_nova3.o tests/$b.o -o tests/$b /opt/fcc/lib/nova/libnova3.a -m tests/$b.map
 	./nova tests/$b tests/$b.map
 done
