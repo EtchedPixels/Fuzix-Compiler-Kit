82ed4b1007d2dc882d255304b4a825fc4a03b4cf
Daniel Gaskell <daniel.gaskell@yale.edu>
2024-08-27 23:10:31 -0700

    fixed typo in _castf primitives

--- support6303/__castf.c
+++ support6303/__castf.c
@@ -7,35 +7,35 @@ unsigned long _castf_ul(uint32_t a1)
     return 0;
 }
 
-unsigned __castf_u(uint32_t a1)
+unsigned _castf_u(uint32_t a1)
 {
     if (a1 & 0x7FFFFFFFUL)
         return MANT(a1) >> (EXP(a1) - EXCESS - 24);
     return 0;
 }
 
-unsigned char __castf_uc(uint32_t a1)
+unsigned char _castf_uc(uint32_t a1)
 {
     if (a1 & 0x7FFFFFFFUL)
         return MANT(a1) >> (EXP(a1) - EXCESS - 24);
     return 0;
 }
 
-long __castf_l(uint32_t a1)
+long _castf_l(uint32_t a1)
 {
     if (a1 == 0)
         return 0;
     if (a1 & 0x80000000)
-        return -__castf_ul(__negatef(a1));
-    return __castf_ul(a1);
+        return -_castf_ul(_negatef(a1));
+    return _castf_ul(a1);
 }
 
-int __castf_(uint32_t a1)
+int _castf_(uint32_t a1)
 {
-    return __castf_l(a1);
+    return _castf_l(a1);
 }
 
-signed char __castf_c(uint32_t a1)
+signed char _castf_c(uint32_t a1)
 {
-    return __castf_l(a1);
+    return _castf_l(a1);
 }
