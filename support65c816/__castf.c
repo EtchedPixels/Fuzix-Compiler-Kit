eaf232bcae538e2ebd8e3b24d4b0e6de33a28d62
Alan Cox <etchedpixels@gmail.com>
2024-10-27 18:54:10 +0000

    __castf.c does not convert correctly
    From: zu2

--- support65c816/__castf.c
+++ support65c816/__castf.c
@@ -2,23 +2,25 @@
 
 unsigned long _castf_ul(uint32_t a1)
 {
-    if (a1 & 0x7FFFFFFFUL)
-        return MANT(a1) >> (EXP(a1) - EXCESS - 24);
-    return 0;
+	int shift;
+    if ((a1 & 0x7FFFFFFFUL)==0)
+		return 0;
+	shift = 24-EXP(a1)+EXCESS;
+	if (shift>=24)
+		return 0;
+	if (shift<0)
+		return MANT(a1)<<-shift;
+	return MANT(a1) >> shift;
 }
 
 unsigned _castf_u(uint32_t a1)
 {
-    if (a1 & 0x7FFFFFFFUL)
-        return MANT(a1) >> (EXP(a1) - EXCESS - 24);
-    return 0;
+	return _castf_ul(a1);
 }
 
 unsigned char _castf_uc(uint32_t a1)
 {
-    if (a1 & 0x7FFFFFFFUL)
-        return MANT(a1) >> (EXP(a1) - EXCESS - 24);
-    return 0;
+	return _castf_ul(a1);
 }
 
 long _castf_l(uint32_t a1)
--- support8080/__castf.c
+++ support8080/__castf.c
@@ -2,40 +2,42 @@
 
 unsigned long _castf_ul(uint32_t a1)
 {
-    if (a1 & 0x7FFFFFFFUL)
-        return MANT(a1) >> (EXP(a1) - EXCESS - 24);
-    return 0;
+	int shift;
+    if ((a1 & 0x7FFFFFFFUL)==0)
+		return 0;
+	shift = 24-EXP(a1)+EXCESS;
+	if (shift>=24)
+		return 0;
+	if (shift<0)
+		return MANT(a1)<<-shift;
+	return MANT(a1) >> shift;
 }
 
-unsigned __castf_u(uint32_t a1)
+unsigned _castf_u(uint32_t a1)
 {
-    if (a1 & 0x7FFFFFFFUL)
-        return MANT(a1) >> (EXP(a1) - EXCESS - 24);
-    return 0;
+	return _castf_ul(a1);
 }
 
-unsigned char __castf_uc(uint32_t a1)
+unsigned char _castf_uc(uint32_t a1)
 {
-    if (a1 & 0x7FFFFFFFUL)
-        return MANT(a1) >> (EXP(a1) - EXCESS - 24);
-    return 0;
+	return _castf_ul(a1);
 }
 
-long __castf_l(uint32_t a1)
+long _castf_l(uint32_t a1)
 {
     if (a1 == 0)
         return 0;
     if (a1 & 0x80000000)
-        return -__castf_ul(__negatef(a1));
-    return __castf_ul(a1);
+        return -_castf_ul(_negatef(a1));
+    return _castf_ul(a1);
 }
 
-int __castf_(uint32_t a1)
+int _castf_(uint32_t a1)
 {
-    return __castf_l(a1);
+    return _castf_l(a1);
 }
 
-signed char __castf_c(uint32_t a1)
+signed char _castf_c(uint32_t a1)
 {
-    return __castf_l(a1);
+    return _castf_l(a1);
 }
