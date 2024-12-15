76b70effa8794ec6ba4e3e829b3c1cdde8b61925
Alan Cox <etchedpixels@gmail.com>
2024-12-15 17:32:54 +0000

    6800: make __pluseqf.c C89 clean and rename for more clarity

--- support6800/__eqopf.c
+++ support6800/__eqopf.c
@@ -1,29 +1,32 @@
 #include "libfp.h"
 
-//
-//	*TOS += high:d
-//
+/*
+ *	*TOS += high:d
+ */
 uint32_t _pluseqf(float a2, float *a1)
 {
 	return (*a1 = *a1 + a2);
 }
-//
-//	*TOS -= high:d
-//
+
+/*
+ *	*TOS -= high:d
+ */
 uint32_t _minuseqf(float a2, float *a1)
 {
 	return (*a1 = *a1 - a2);
 }
-//
-//	*TOS *= high:d
-//
+
+/*
+ *	*TOS *= high:d
+ */
 uint32_t _muleqf(float a2, float *a1)
 {
 	return (*a1 = *a1 * a2);
 }
-//
-//	*TOS /= high:d
-//
+
+/*
+ *	*TOS /= high:d
+ */
 uint32_t _diveqf(float a2, float *a1)
 {
 	return (*a1 = *a1 / a2);
