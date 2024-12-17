10dbcf09cef56f78c56759f24f4ea1ae20f744ce
Alan Cox <etchedpixels@gmail.com>
2024-12-17 19:31:17 +0000

    8070: fix test run script

--- test/run-test8070.sh
+++ test/run-test8070.sh
@@ -5,6 +5,6 @@ do
 	b=$(basename $i .c)
 	echo  $b":"
 	fcc -X -m8070 -c tests/$b.c
-	ld8070 -b -C1 -m tests/$b.map -o tests/$b testcrt0_8070.o tests/$b.o /opt/fcc/lib/8070/lib8070.a
+	ld8070 -b -C1 -Z0xFF00 -m tests/$b.map -o tests/$b testcrt0_8070.o tests/$b.o /opt/fcc/lib/8070/lib8070.a
 	./emu807x tests/$b tests/$b.map
 done
