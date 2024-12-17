086d1e5511b234e01e38eb7b23c05854f2533f1a
Alan Cox <etchedpixels@gmail.com>
2024-12-17 18:57:26 +0000

    8070: add switch helpers

---
+++ support8070/__switch.s
@@ -0,0 +1,24 @@
+
+	.export __switch
+;
+;	P3 points to our table
+;
+	ld ea,2,@p3
+	st a,:__tmp		; count
+	bz match
+;
+;	Walk the entries comparing with EA
+;
+	ld t,ea			; save the value we seek
+
+loop:
+	ld ea,t
+	sub ea,2,@p3		; check if matches
+	or a,e
+	bz match
+	ld ea,2,@p3		; skip address
+	dld a,:__tmp
+	bnz loop
+match:
+	ld p0,2,@p3		; get the function address into PC
+
